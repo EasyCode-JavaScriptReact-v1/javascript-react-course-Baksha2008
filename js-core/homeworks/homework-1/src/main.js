77562a008ae7decdf4abb0eee7ce111b3af18d5b
baksha2008 <baksha2008@gmail.com>
2018-06-01 07:15:14 +0300

    homework-1

---
+++ js-core/homeworks/homework-1/src/main.js
@@ -0,0 +1,26 @@
+let phoneBook = {
+    subscribersCard: {
+        firstName: 'Yulya',
+        secondName: 'Ivanova',
+        photo: undefined,
+        phoneNumber: {
+            main: +380633888930,
+            add: null
+        },
+        emailAddress: 'yulya@gamil.com',
+        group: 'friends',
+        notes: null,
+        ringtones: [
+            'standart:true',
+            'rington1:false',
+            'rington12:false'
+        ],
+        address: [
+            'street: "23 serpnya"',
+            'home: "10"',
+            'apartmen: "82"'
+        ]
+    }    
+};
+
+console.log(phoneBook);
